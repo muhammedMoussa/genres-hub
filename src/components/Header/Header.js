59fb4bd9e68605b07632aa6a25dca38297d0324d
muhammedMoussa <muhammedmoussa@hotmail.com>
2020-09-19 14:14:55 +0200

    usability: added home button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,45 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Button, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Button, Typography, makeStyles } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+
+import './styles.css';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 function Header() {
+    const classes = useStyles();
+
     return (
-        <div>
+        <header>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start"  color="inherit" aria-label="menu">
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" >
+
+                    <Typography variant="h6" className={classes.title}>
                         GenresHub
                     </Typography>
+                    <Button color="inherit">
+                        <Link to="/"><HomeIcon /></Link>
+                    </Button>
                     {/* @TODO: HANDLE AUTH */}
                     {/* <Button color="inherit">Login</Button> */}
                 </Toolbar>
             </AppBar>
-        </div>
+        </header>
     )
 }
 
