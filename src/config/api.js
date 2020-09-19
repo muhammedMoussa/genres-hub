a4d0e3103c2f8c0a8e128c84517685b7e3999aef
muhammedMoussa <muhammedmoussa@hotmail.com>
2020-09-19 13:33:59 +0200

    update: api endpoints

--- src/config/api.js
+++ src/config/api.js
@@ -2,4 +2,6 @@ const CORS_HELPER = 'https://cors-anywhere.herokuapp.com';
 
 export const api = {
     GENRS_API: `${CORS_HELPER}/https://api.deezer.com/genre`,
+    GENR_CHART_API: `${CORS_HELPER}/https://api.deezer.com/chart`,
+    ARTIST_API: `${CORS_HELPER}/https://api.deezer.com/artist`,
 }
